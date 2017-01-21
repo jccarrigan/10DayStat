b4a0bce408b05248a4f47f9a3d905a27c13e35d9
blairdev1023 <blairdev1023@gmail.com>
2017-01-20 17:30:48 -0700

    Create Day5-PoissonDistributionII.py

---
+++ Day5-PoissonDistributionII.py
@@ -0,0 +1,5 @@
+print 160.0 + 40.0 * ( 0.88 + pow(0.88, 2))
+print 128.0 + 40.0 * ( 1.55 + pow(1.55, 2))
+
+
+# for what's it's worth, this exercise (as many are in the 10 days of stats) is worthless in teaching stats and this problem does not make sense.
